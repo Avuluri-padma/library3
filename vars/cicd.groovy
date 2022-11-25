02605e56c15ae6480eb973e41211c57126236e51
Avuluri-padma <118344333+Avuluri-padma@users.noreply.github.com>
2022-11-25 12:04:30 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def newDownload(repo)
 {
   sh 'mvn package'
 }
+def newDeploy(jobname,ip,appname)
+{
+  sh "scp/var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
